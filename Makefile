8b9ec5bb5266aaba78fa65cb0a39d3837b07c04c
Igor Lipovtsev <ilip.z@ya.ru>
2020-10-04 10:26:50 +0700

    Finish 1st task

---
+++ Makefile
@@ -0,0 +1,48 @@
+PJ_FLAG =  `pkg-config --cflags --libs libpjproject`
+ARM_CC = arm-linux-gnueabihf-gcc-5 -c 
+CC = gcc  -c
+
+
+all: task1 task2 task3 task4 task1-arm task2-arm task3-arm task4-arm
+
+
+# For current platform: x86_64
+
+task1: 
+	$(CC) task1.c 
+	gcc task1.o -o task1 $(PJ_FLAG)
+
+task2:
+	$(CC) task2.c 
+	gcc task2.o -o task2 $(PJ_FLAG)
+
+task3: 
+	$(CC) task3.c 
+	gcc task3.o -o task3 $(PJ_FLAG)
+
+task4:
+	$(CC) task4.c 
+	gcc task4.o -o task4 $(PJ_FLAG)
+
+task1-arm:
+	$(ARM_CC) task1.c 
+	gcc task1-arm.o -o task1-arm $(PJ_FLAG)
+
+task2-arm:
+	$(ARM_CC) task2.c 
+	gcc task2-arm.o -o task2-arm $(PJ_FLAG)
+
+task3-arm:
+	$(ARM_CC) task3.c 
+	gcc task3-arm.o -o task3-arm $(PJ_FLAG)
+
+task4-arm:
+	$(ARM_CC) task4.c 
+	gcc task4-arm.o -o task4-arm $(PJ_FLAG)
+
+.PHONY: clean
+	
+clean:
+	rm *.o 
+	rm task1 task1-arm task2 task2-arm task3 task3-arm task4 task4-arm
+
---
+++ task1.c
@@ -0,0 +1,123 @@
+#include <pjsua-lib/pjsua.h>
+#include <unistd.h>
+
+#define THIS_FILE	"APP"
+
+#define SIP_DOMAIN	"example.com"
+#define SIP_USER	"alice"
+#define SIP_PASSWD	"secret"
+
+
+int done = 1;
+
+/* Callback called by the library upon receiving incoming call */
+static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id,
+			     pjsip_rx_data *rdata)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(acc_id);
+    PJ_UNUSED_ARG(rdata);
+
+    pjsua_call_get_info(call_id, &ci);
+
+    PJ_LOG(3,(THIS_FILE, "Incoming call from %.*s!!",
+			 (int)ci.remote_info.slen,
+			 ci.remote_info.ptr));
+
+    /* Automatically answer incoming calls with 200/OK */
+    pjsua_call_answer(call_id, 200, NULL, NULL);
+    
+    sleep (10);
+	
+    pjsua_call_hangup_all();
+    
+    done = 0;
+}
+
+/* Callback called by the library when call's state has changed */
+static void on_call_state(pjsua_call_id call_id, pjsip_event *e)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(e); 							// do not warn about unused arg - event *e
+
+    pjsua_call_get_info(call_id, &ci);
+    PJ_LOG(3,(THIS_FILE, "Call %d state=%.*s", call_id,
+			 (int)ci.state_text.slen,
+			 ci.state_text.ptr));
+}
+
+/* Display error and exit application */
+static void error_exit(const char *title, pj_status_t status)
+{
+    pjsua_perror(THIS_FILE, title, status);
+    pjsua_destroy();
+    exit (1);
+}
+
+int main(int argc, char *argv[])
+{
+    pjsua_acc_id acc_id;
+    pj_status_t status;
+    pj_log_set_decor (PJ_LOG_HAS_COLOR);
+    /* Create pjsua first! */
+    status = pjsua_create();
+    if (status != PJ_SUCCESS) error_exit("Error in pjsua_create()", status);
+
+    /* Init pjsua */
+    {
+	pjsua_config cfg;
+	pjsua_logging_config log_cfg;
+
+	pjsua_config_default(&cfg);
+	cfg.cb.on_incoming_call = &on_incoming_call; 
+	cfg.cb.on_call_state = &on_call_state;
+
+	pjsua_logging_config_default(&log_cfg);
+	log_cfg.console_level = 4;
+
+	status = pjsua_init(&cfg, &log_cfg, NULL);
+	if (status != PJ_SUCCESS) error_exit("Error in pjsua_init()", status);
+    }
+
+    /* Add UDP transport. */
+    {
+	pjsua_transport_config cfg;
+
+	pjsua_transport_config_default(&cfg);
+
+	cfg.port = 5060;
+	status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &cfg, NULL);
+	if (status != PJ_SUCCESS) error_exit("Error creating transport", status);
+    }
+
+    /* Initialization is done, now start pjsua */
+    status = pjsua_start();
+    if (status != PJ_SUCCESS) error_exit("Error starting pjsua", status);
+
+    /* Register to SIP server by creating SIP account. */
+    {
+	pjsua_acc_config cfg;
+	
+	pjsua_acc_config_default(&cfg);
+	cfg.id = pj_str("sip:" SIP_USER "@" SIP_DOMAIN);
+	cfg.reg_uri = pj_str("sip:" SIP_DOMAIN); 
+	cfg.cred_count = 1;
+	cfg.cred_info[0].realm = pj_str(SIP_DOMAIN); 
+	cfg.cred_info[0].scheme = pj_str("digest"); 			// using hash sums
+	cfg.cred_info[0].username = pj_str(SIP_USER);
+	cfg.cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD; 	// authentication type - password
+	cfg.cred_info[0].data = pj_str(SIP_PASSWD); 			// password
+
+	status = pjsua_acc_add(&cfg, PJ_TRUE, &acc_id);
+	if (status != PJ_SUCCESS) error_exit("Error adding account", status);
+    } 
+
+    while (done)
+	    sleep (1);
+
+    pjsua_destroy();
+
+    return 0;
+}
